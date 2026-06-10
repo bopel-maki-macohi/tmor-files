0c6307c088a97fdeb6bb8123614369da30a72fa0
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-06-10 15:32:44 -0500

    minor fixes

--- build-html5.bat
+++ build-html5.bat
@@ -1,4 +1,4 @@
-REM https://tutorialreference.com/batch-scripting/examples/faq/batch-script-how-to-get-current-date-time-into-variables
+@REM https://tutorialreference.com/batch-scripting/examples/faq/batch-script-how-to-get-current-date-time-into-variables
 @ECHO OFF
 SET "YYYY=%DATE:~10,4%"
 SET "MM=%DATE:~4,2%"
@@ -8,7 +8,7 @@ SET "HH=%TIME:~0,2%"
 SET "MIN=%TIME:~3,2%"
 SET "SEC=%TIME:~6,2%"
 
-REM Handle the leading space in the time for hours < 10
+@REM Handle the leading space in the time for hours < 10
 IF "%HH:~0,1%"==" " SET "HH=0%HH:~1,1%"
 
 SET "SortableDateTime=%YYYY%-%MM%-%DD%@%HH%-%MIN%-%SEC%"
