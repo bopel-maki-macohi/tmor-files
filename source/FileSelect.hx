171e9784390a2d72f5292eea19fd3f76e2b5a503
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-06-10 15:58:18 -0500

    alright im bored

--- source/FileSelect.hx
+++ source/FileSelect.hx
@@ -1,3 +1,6 @@
+import flixel.util.FlxColor;
+import flixel.text.FlxText;
+import flixel.group.FlxGroup.FlxTypedGroup;
 import haxe.Json;
 import lime.utils.Assets;
 import flixel.FlxState;
@@ -8,10 +11,41 @@ class FileSelect extends FlxState
 {
 	var files:Array<Entry> = [];
 
+	var textGroup:FlxTypedGroup<FlxText>;
+	var selectextFile:Int = 0;
+
 	override function create()
 	{
 		super.create();
 
+		getFiles();
+
+		textGroup = new FlxTypedGroup<FlxText>();
+		add(textGroup);
+
+		for (i => file in files)
+		{
+			var newText:FlxText = new FlxText(0, 0, 0, file.name, 16);
+			newText.ID = i;
+			textGroup.add(newText);
+		}
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		for (text in textGroup)
+		{
+			text.y = text.ID * (text.size * 2);
+
+			text.color = FlxColor.WHITE;
+			if (selectextFile == text.ID) text.color = FlxColor.YELLOW;
+		}
+	}
+
+	function getFiles()
+	{
 		for (file in Assets.list(TEXT))
 		{
 			if (file.extension() != 'json')
